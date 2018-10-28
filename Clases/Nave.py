f849ee9ed1f0b1085919b0539ec5579c36f391d7
Fernando Flores <fernandondin@ciencias.unam.mx>
2018-10-28 17:56:34 -0600

    Clases

---
+++ Clases/Nave.py
@@ -0,0 +1,27 @@
+import pygame
+import Proyectil
+class naveEspacial(pygame.sprite.Sprite):
+    def __init__(self,ancho,alto):
+        pygame.sprite.Sprite.__init__(self)
+        self.ImagenNave=pygame.image.load('/home/fernandondin/pygame/Imagenes/nave.png')
+        self.ImagenExplosion=pygame.image.load('/home/fernandondin/pygame/Imagenes/marciano.png')
+        self.rect=self.ImagenNave.get_rect()
+        self.rect.centerx = ancho/2
+        self.rect.centery = alto-35
+        self.listaDisparo=[]
+        self.Vida=True
+        self.velocidad=20
+        pygame.mixer.init()  
+        self.sonido = pygame.mixer.Sound('/home/fernandondin/pygame/Sonidos/disparo3.wav')
+        self.sonidoExplosion = pygame.mixer.Sound('/home/fernandondin/pygame/Sonidos/Explosion.wav')
+    def dispara(self,x,y):
+        proyectil = Proyectil.Proyectil(x,y,"/home/fernandondin/pygame/Imagenes/proyectil.png",True)
+        self.listaDisparo.append(proyectil)
+        self.sonido.play()
+    def dibujar(self,superficie):
+        superficie.blit(self.ImagenNave,self.rect)
+    def destruccion(self):
+        self.sonidoExplosion.play()
+        self.vida=False
+        self.velocidad=0
+        self.imagenNave = self.ImagenExplosion
