d6de86ad72a0c669740c426dd079eb6e95b7dafa
Fernando Flores <fernandondin@ciencias.unam.mx>
2018-10-28 19:34:22 -0600

    Clases

--- Clases/Nave.py
+++ Clases/Nave.py
@@ -3,8 +3,8 @@ import Proyectil
 class naveEspacial(pygame.sprite.Sprite):
     def __init__(self,ancho,alto):
         pygame.sprite.Sprite.__init__(self)
-        self.ImagenNave=pygame.image.load('../Imagenes/nave.png')
-        self.ImagenExplosion=pygame.image.load('../Imagenes/marciano.png')
+        self.ImagenNave=pygame.image.load('Imagenes/nave.png')
+        self.ImagenExplosion=pygame.image.load('Imagenes/Marciano.png')
         self.rect=self.ImagenNave.get_rect()
         self.rect.centerx = ancho/2
         self.rect.centery = alto-35
@@ -12,10 +12,10 @@ class naveEspacial(pygame.sprite.Sprite):
         self.Vida=True
         self.velocidad=20
         pygame.mixer.init()  
-        self.sonido = pygame.mixer.Sound('../Sonidos/disparo3.wav')
-        self.sonidoExplosion = pygame.mixer.Sound('../Sonidos/Explosion.wav')
+        self.sonido = pygame.mixer.Sound('Sonidos/disparo3.wav')
+        self.sonidoExplosion = pygame.mixer.Sound('Sonidos/Explosion.wav')
     def dispara(self,x,y):
-        proyectil = Proyectil.Proyectil(x,y,"../Imagenes/proyectil.png",True)
+        proyectil = Proyectil.Proyectil(x,y,"Imagenes/proyectil.png",True)
         self.listaDisparo.append(proyectil)
         self.sonido.play()
     def dibujar(self,superficie):
